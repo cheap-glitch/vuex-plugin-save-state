d16bd315dc3063f2c1d35085a2e7ee3f9c7e2b92
cheap-glitch <cheap.glitch@gmail.com>
2020-09-22 12:05:42 +0200

    ♻️  refactor: Simplify the code
    Replace calls to `Array.reduce()` with simpler equivalents
    and rearrange the flow of the line breaks to be more legible.

--- index.js
+++ index.js
@@ -24,37 +24,30 @@ export function saveStatePlugin(stateModel, options = {})
 	return function(store)
 	{
 		// Declare a function to check that a prop has been stored and that the stored value passes validation
-		const checkProp = (prop, storedValue) => storedValue && (prop.saved === undefined ? options.savedByDefault : prop.saved) && (!prop.validator || prop.validator(storedValue));
+		const checkProp = (prop, storedValue) => storedValue
+			&& (prop.saved === undefined ? options.savedByDefault : prop.saved)
+			&& (!prop.validator || prop.validator(storedValue));
 
 		// Get the stored state
 		let storedState = {};
-		try {
-			storedState = JSON.parse(localStorage.getItem(options.namespace) || '{}');
-		}
-		catch (err) {
-			storedState = {};
-		}
+		try { storedState = JSON.parse(localStorage.getItem(options.namespace) || '{}'); } catch { storedState = {}; }
 
-		// Filter it and deep merge it with the default state
-		store.replaceState(Object.keys(stateModel).reduce(function(newState, key)
-		{
-			const prop = stateModel[key];
+		// Filter it and do a deep merge with the default state
+		store.replaceState(Object.fromEntries(stateModel)
 
-			if (!prop || Object.prototype.toString.call(prop) !== '[object Object]')
-				return newState;
+			// Remove non-object properties
+			.filter(([key, prop]) => !!prop && Object.prototype.toString.call(prop) !== '[object Object]')
 
-			// Property
-			if (Object.keys(prop).every(k => ['saved', 'default', 'validator'].includes(k)))
-				newState[key] = checkProp(prop, storedState[key]) ? storedState[key] : prop.default;
+			.map(([key, prop]) => (Object.keys(prop).every(k => ['saved', 'default', 'validator'].includes(k)))
 
-			// Module
-			else newState[key] = Object.keys(prop).reduce((newModule, subkey) => {
-				newModule[subkey] = checkProp(prop[subkey], (storedState[key] || {})[subkey]) ? storedState[key][subkey] : prop[subkey].default;
-				return newModule;
-			}, {});
+				// Add a store property
+				? [key, checkProp(prop, storedState[key]) ? storedState[key] : prop.default];
 
-			return newState;
-		}, {}));
+				// Add a store module
+				: [key, Object.fromEntries(Object.entries(prop).map(
+					([subkey, subprop]) => [subkey, checkProp(subprop, (storedState[key] || {})[subkey]) ? storedState[key][subkey] : subprop.default]
+				))];
+		));
 
 		// Subscribe to the store and save the state upon every mutation
 		store.subscribe((_, state) => localStorage.setItem(options.namespace, JSON.stringify(state)));
@@ -67,5 +60,5 @@ export function saveStatePlugin(stateModel, options = {})
  */
 export function getVuexState(state)
 {
-	return Object.keys(state).reduce((result, key) => { result[key] = state[key].default || null; return result; }, {});
+	return Object.fromEntries(Object.entries(state).map(([key, value]) => [key, value.default || null]));
 }
